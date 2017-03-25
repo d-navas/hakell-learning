012b4fbda992de6fc52e3a762f13a6df67ac48be
Danilo Navas <dnavas@fusionapps.com>
2017-03-24 22:41:46 -0400

    functor 16.6

--- haskellbook/ch16/TheFunctor.hs
+++ haskellbook/ch16/TheFunctor.hs
@@ -7,6 +7,11 @@ module TheFunctor where
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 16.3: There's a whole lot of fmap going round
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~
+data FixMePls a = FixMe | Pls a deriving (Eq, Show)
+
+instance Functor FixMePls where
+  fmap _ FixMe = FixMe
+  fmap f (Pls a) = Pls (f a)
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 16.5: Functor Laws
@@ -18,11 +23,24 @@ module TheFunctor where
 -- Composition
 -- fmap (f . g) = fmap f . fmap g
 
-data FixMePls a = FixMe | Pls a deriving (Eq, Show)
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- 16.6: The Good, the Bad, and the Ugly
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~
+data WhoCares a = ItDoesnt | Matter a | WhatThisIsCalled deriving (Eq, Show)
 
-instance Functor FixMePls where
-  fmap _ FixMe = FixMe
-  fmap f (Pls a) = Pls (f a)
+instance Functor WhoCares where
+  fmap _ ItDoesnt = ItDoesnt
+  fmap _ WhatThisIsCalled = WhatThisIsCalled
+  fmap f (Matter a) = Matter (f a)
 
+-- Law breaking instance. Structure is broken
+-- instance Functor WhoCares where
+--   fmap _ ItDoesnt = WhatThisIsCalled
+--   fmap f WhatThisIsCalled = ItDoesnt
+--   fmap f (Matter a) = Matter (f a)
+
+-- ~~~~~~~~~~
+-- Main
+-- ~~~~~~~~~~
 main :: IO ()
 main = return ()
