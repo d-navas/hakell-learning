4e93035fd3362a29d2d0566ad18416fd044e3b68
Danilo Navas <dnavas@fusionapps.com>
2017-04-01 13:08:14 -0400

    complete 16.10 exercises

--- haskellbook/ch16/TheFunctor.hs
+++ haskellbook/ch16/TheFunctor.hs
@@ -112,6 +112,47 @@ functorCompose' x (Fun _ f) (Fun _ g) = (fmap (g . f) x) == (fmap g . fmap f $ x
 type IntToInt = Fun Int Int
 type IntFC = [Int] -> IntToInt -> IntToInt -> Bool
 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- 16.10: Exercises: Implement Functor instances
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- ~~~~~ 1:
+newtype Identity a = Identity a deriving (Eq, Show)
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+-- ~~~~~ 2:
+data Pair a = Pair a a deriving (Eq, Show)
+instance Functor Pair where
+  fmap f (Pair a a') = Pair (f a) (f a')
+
+-- ~~~~~ 3:
+-- Was already done by the book earlier
+
+-- ~~~~~ 4:
+data Three a b c = Three a b c deriving (Eq, Show)
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+-- ~~~~~ 5:
+data Three' a b = Three' a b b deriving (Eq, Show)
+instance Functor (Three' a) where
+  fmap f (Three' a b b') = Three' a (f b) (f b')
+
+-- ~~~~~ 6:
+data Four a b c d = Four a b c d deriving (Eq, Show)
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+-- ~~~~~ 7:
+data Four' a b = Four' a a a b deriving (Eq, Show)
+instance Functor (Four' a) where
+  fmap f (Four' a a' a'' b) = Four' a a' a'' (f b)
+
+-- ~~~~~ 8:
+data Trivial = Trivial --can't be implemented since it's kind * and Functor takes * -> *
+
+
+
 -- Main
 main :: IO ()
 main = do
@@ -119,5 +160,4 @@ main = do
   quickCheck $ \x -> functorIdentity (x :: [Int])
   quickCheck $ \x -> functorCompose (+1) (*2) (x :: [Int])
   quickCheck (functorCompose' :: IntFC)
-
 
