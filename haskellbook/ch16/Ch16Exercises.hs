0e8d11e7d274bbfb6a0657adcc3dd0cd73517565
Danilo Navas <dnavas@fusionapps.com>
2017-04-12 23:53:59 -0400

    complete ch16 exercises

--- haskellbook/ch16/Ch16Exercises.hs
+++ haskellbook/ch16/Ch16Exercises.hs
@@ -73,10 +73,61 @@ newtype Flip f a b = Flip (f b a)
 newtype K' a b = K' a
 
 instance Functor (Flip K' a) where
-  fmap = undefined
+  fmap f (Flip (K' a)) = Flip $ K' (f a)
 
+-- 4.
+newtype EvilGoateeConst a b = GoatyConst b
 
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+-- 5.
+newtype LiftItOut f a = LiftItOut (f a)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut fa) = LiftItOut (fmap f fa)
+
+-- 6.
+data Parappa f g a = DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa fa ga) = DaWrappa (f <$> fa) (f <$> ga)
+
+-- 7.
+data IgnoreOne f g a b = IngnoringSomething (f a) (g b)
+
+instance (Functor f, Functor g) => Functor (IgnoreOne f g a) where
+  fmap f (IngnoringSomething fa gb) = IngnoringSomething fa (f <$> gb)
+
+-- 8.
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance (Functor g) => Functor (Notorious g o a) where
+  fmap f (Notorious go ga gt) = Notorious go ga (f <$> gt)
+
+-- 9.
+data List a = Nil | Cons a (List a)
+
+instance Functor List where
+  fmap _ Nil= Nil
+  fmap f (Cons a la) = Cons (f a) (f <$> la)
+
+-- 10.
+data GoatLord a = NoGoat | OneGoat a | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+
+instance Functor GoatLord where
+  fmap _ NoGoat = NoGoat
+  fmap f (OneGoat a) = OneGoat (f a)
+  fmap f (MoreGoats fa fb fc) = MoreGoats (f <$> fa) (f <$> fb) (f <$> fc)
+
+-- 11.
+data TalkToMe a = Halt | Print String a | Read (String -> a)
+
+instance Functor TalkToMe where
+  fmap _ Halt = Halt
+  fmap f (Print t a) = Print t (f a)
+  fmap f (Read h) = Read (f . h)
 
 
 main :: IO ()
-main = putStrLn "hello"
+main = putStrLn "Ch16 Exercises"
