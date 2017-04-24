adbed6ba50aeba23a82f4c110af388159e694ecd
Danilo Navas <dnavas@fusionapps.com>
2017-04-23 23:41:54 -0400

    complete 17.7 and start 17.8

--- haskellbook/ch17/Applicatives.hs
+++ haskellbook/ch17/Applicatives.hs
@@ -1,71 +1,75 @@
 module Applicatives where
 
 import Data.List (elemIndex)
+import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers -- provided by external library
+import Test.QuickCheck.Classes -- provided by external library
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 17.2: Defining Applicative
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~
 -- So, every type that can have an Applicative instance must also have a Functor instance
--- class Functor f => Applicative f where
---   pure  :: a -> f a
---   (<*>) :: f (a -> b) -> f a -> f b
+class Functor f => Applicative f where
+  pure  :: a -> f a
+  (<*>) :: f (a -> b) -> f a -> f b
 
 -- Core Control.Applicative functions
--- liftA :: Applicative f => (a -> b)
---           -> f a
---           -> f b
+liftA :: Applicative f => (a -> b)
+          -> f a
+          -> f b
 
--- liftA2 :: Applicative f => (a -> b -> c)
---           -> f a
---           -> f b
---           -> f c
+liftA2 :: Applicative f => (a -> b -> c)
+          -> f a
+          -> f b
+          -> f c
 
--- liftA3 :: Applicative f => (a -> b -> c -> d)
---           -> f a
---           -> f b
---           -> f c
---           -> f d
+liftA3 :: Applicative f => (a -> b -> c -> d)
+          -> f a
+          -> f b
+          -> f c
+          -> f d
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 17.3: Functor vs Applicative
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- fmap (+1) [1..3] is equivalent to pure (+1) <*> [1..3]
+fmap (+1) [1..3] is equivalent to pure (+1) <*> [1..3]
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 17.4: Applicative Functors are monoidal functors
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- mappend :: Monoid a => a -> a -> a
+mappend :: Monoid a => a -> a -> a
 
 {- Show me the monoids -}
 
 {- Tuple Monoid nad Applicative side by side -}
--- instance (Monoid a, Monoid b) => Monoid (a, b) where
---   mempty = (mempty, mempty)
---   (a, b) `mappend` (a', b') = (a `mappend` a', b `mappend` b')
+instance (Monoid a, Monoid b) => Monoid (a, b) where
+  mempty = (mempty, mempty)
+  (a, b) `mappend` (a', b') = (a `mappend` a', b `mappend` b')
 
--- instance Monoid a => Applicative ((,) a) where
---   pure x = (mempty, x)
---   (u, f) <*> (v, x) = (u `mappend` v, f x)
+instance Monoid a => Applicative ((,) a) where
+  pure x = (mempty, x)
+  (u, f) <*> (v, x) = (u `mappend` v, f x)
 
 {- Maybe Monoid and Applicative -}
--- instance Monoid a => Monoid a (Maybe a) where
---   mempty = Nothing
---   mappend m Nothing = m
---   mappend Nothing m = m
---   mappend (Just a) (Just a') = Just (a `mappend` a')
+instance Monoid a => Monoid a (Maybe a) where
+  mempty = Nothing
+  mappend m Nothing = m
+  mappend Nothing m = m
+  mappend (Just a) (Just a') = Just (a `mappend` a')
 
--- instance Applicative Maybe where -- implicit Monoid (see it?)
---   pure = Just
---   Nothing <*> _ = Nothing
---   Just f <*> Just a = Just (f a)
+instance Applicative Maybe where -- implicit Monoid (see it?)
+  pure = Just
+  Nothing <*> _ = Nothing
+  Just f <*> Just a = Just (f a)
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 17.5: Applicative in Use
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- f x = lookup x [(3, "hello"), (4, "julie"), (5, "kbai")]
--- g y = lookup y [(7, "sup?"), (8, "chris"), (9, "aloha")]
--- h z = lookup z [(2, 3), (5, 6), (7, 8)]
--- m x = lookup x [(4, 10), (8, 13), (1, 9001)]
+f x = lookup x [(3, "hello"), (4, "julie"), (5, "kbai")]
+g y = lookup y [(7, "sup?"), (8, "chris"), (9, "aloha")]
+h z = lookup z [(2, 3), (5, 6), (7, 8)]
+m x = lookup x [(4, 10), (8, 13), (1, 9001)]
 
 {- Exercises -}
 -- Make following expressions typecheck
@@ -152,41 +156,71 @@ mkPerson :: String -> String -> Maybe Person
 mkPerson n a = Person <$> mkName n <*> mkAddress a
 
 -- Exersise: Fixer Upper
--- 1. const <$> Just "Hello" <*> pure "World"
--- 2. (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
+const <$> Just "Hello" <*> pure "World"
+(,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1, 2, 3]
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -- 17.6: Applicative Laws
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 -- 1. Identity:
-  -- pure id <*> v = v
+  pure id <*> v = v
 
   -- Example (all equal):
-  -- id [1..5]
-  -- fmap id [1..5]
-  -- pure id <*> [1..5]
+  id [1..5]
+  fmap id [1..5]
+  pure id <*> [1..5]
 
 -- 2. Composition:
-  -- pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
+  pure (.) <*> u <*> v <*> w = u <*> (v <*> w)
 
 -- 3. Homomorphism:
-  -- pure f <*> pure x = pure (f x)
+  pure f <*> pure x = pure (f x)
 
 -- e.g.:
-  -- pure (+1) <*> pure 1 = pure ((+1) 1)
+  pure (+1) <*> pure 1 = pure ((+1) 1)
 
 -- 4. Interchange:
-  -- u <*> pure y = pure ($ y) <*> u
+  u <*> pure y = pure ($ y) <*> u
 
   -- (u: function embedded in some strcuture)
   -- e.g.
-  -- Just (+2) <*> pure 2
+  Just (+2) <*> pure 2
 
   -- According to the law, following are equal:
-  -- Just (+2) <*> pure 2 == pure ($ 2) <*> Just (+2)
+  Just (+2) <*> pure 2 == pure ($ 2) <*> Just (+2)
 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- 17.7: QuickChecking the Applicative Laws
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+data Bull = Fools | Twoo deriving (Eq, Show)
 
+instance Arbitrary Bull where
+  arbitrary = frequency [ (1, return Fools)
+                        , (1, return Twoo)]
+
+instance Monoid Bull where
+  mempty = Fools
+  mappend _ _ = Fools
+
+instance EqProp Bull where (=-=) = eq
 
 main :: IO ()
-main = putStrLn "Ch-17: Applicatives"
+main = quickBatch (monoid Twoo)
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- 17.8: ZipList Monoid
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+
+
+
+
+
+
+
+
