7cf3f3973ce2aaa11bfe62013c8fa857ac5f4cd5
Danilo Navas <dnavas@fusionapps.com>
2017-04-24 23:52:00 -0400

    complete 17.8 ZipList Monoid

--- haskellbook/ch17/Applicatives.hs
+++ haskellbook/ch17/Applicatives.hs
@@ -212,15 +212,43 @@ main = quickBatch (monoid Twoo)
 -- 17.8: ZipList Monoid
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+instance Monoid a => Monoid (ZipList a) where
+  mempty = pure mempty
+  mappend = liftA2 mappend
 
+instance Arbitrary a => Arbitrary (ZipList a) where
+  arbitrary = ZipList <$> arbitrary
 
+instance Arbitrary a => Arbitrary (Sum a) where
+  arbitrary = Sum <$> arbitrary
 
+instance Eq a => EqProp (ZipList a) where (=-=) = eq
 
+-- List Applicative Exercise:
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
 
+append :: List a -> List a -> List a
+append Nil ys         = ys
+append (Cons x xs) ys = Cons x (append xs ys)
 
+fold :: (a -> b -> b) -> b -> List a -> b
+fold _ b Nil        = b
+fold f b (Cons h t) = f h (fold f b t)
 
+concat' :: List (List a) -> List a
+concat' = fold append Nil
 
+flatMap :: (a -> List b) -> List a -> List b
+flatMap f as = concat' $ fmap f as
 
+instance Functor List where
+  fmap f Nil = Nil
+  fmap f (Cons a as) = Cons (f a) (fmap f as)
 
-
+instance Applicative List where
+  pure a = Cons a Nil
+  (Cons f Nil) <*> (Cons a Nil) = Cons (f a) Nil
+  Cons f fs <*> xs = append (f <$> xs) (fs <*> xs)
+  _ <*> Nil = Nil
+  Nil <*> _ = Nil
 
