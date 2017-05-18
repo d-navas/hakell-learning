f3e767aeded6ede992564941c8332bc692dd0fca
Danilo Navas <dnavas@fusionapps.com>
2017-05-17 22:06:23 -0400

    tuple traversable instance

--- haskellbook/ch21/TheTraversable.hs
+++ haskellbook/ch21/TheTraversable.hs
@@ -134,6 +134,29 @@ instance Traversable (Either a) where
   traverse _ (Left x) = pure (Left x)
   traverse f (Right y) = Right <$> f y
 
+-- Tuple
+instance Functor ((,) a) where
+  fmap f (x, y) = (x, f y)
+
+instance Monoid a => Applicative ((,) a) where
+  pure x = (mempty, x)
+  (u, f) <*> (v, x) = (u `mappend` v, f x)
+
+instance Foldable ((,) a) where
+  foldMap f (_, y) = f y
+  foldr f z (_, y) = f y z
+
+instance Traversable ((,) a) where
+  traverse f (x, y) = (,) x <$> f y
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- 21.10: Traversable Laws
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
 
 
 -- main
